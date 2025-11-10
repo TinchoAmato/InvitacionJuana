5b096d7f1464492228658515564dc14d46f7fd41
Martín Amato <amatomartin.ma@gmail.com>
2025-11-10 01:09:25 -0300

    Actualizo estilos y script con ajustes de audio y fondo 2

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const playButton = document.getElementById("playButton");
 const playIcon = document.getElementById("playIcon");
 
 // 🎶 Tu canción (podés reemplazarla por una local en /audio/ si querés)
-const audio = new Audio("./audio/coldplay.m4a");
+const audio = new Audio("Audio/Coldplay.m4a");
 
 let isPlaying = false;
 
